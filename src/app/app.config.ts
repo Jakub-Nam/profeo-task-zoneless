3b9fa6fabfb31319e99c9e4673458c7cc69d3823
Jakub Namysl <kubaNam@gmail.com>
2025-09-22 15:40:09 +0200

    add injection token for base api url

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,22 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
+import { API_BASE_URL } from './tokens/api.tokens';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideHttpClient(),
+    {
+      provide: API_BASE_URL,
+      useValue: 'https://jsonplaceholder.typicode.com',
+    },
+  ],
 };
