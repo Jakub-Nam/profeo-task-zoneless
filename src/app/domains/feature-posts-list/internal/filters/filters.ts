c605db3bc1768e01b5fd265166bbbc078b98bc3e
Jakub Namysl <kubaNam@gmail.com>
2025-09-22 18:48:43 +0200

    clean filters

--- src/app/domains/feature-posts-list/internal/filters/filters.ts
+++ src/app/domains/feature-posts-list/internal/filters/filters.ts
@@ -10,15 +10,12 @@ import { IUser } from '../../../shared/models/user.interface';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Filters {
-  // Input for users list
   public readonly users = input<IUser[]>([]);
 
-  // Filters for content search and user selection
   protected readonly filterText = signal('');
   protected readonly userIdFilter = signal<number | null>(null);
   protected readonly showOnlyFavorites = signal(false);
 
-  // Outputs for filter changes
   public readonly filterChange = output<string>();
   public readonly userIdFilterChange = output<number | null>();
   public readonly showOnlyFavoritesChange = output<boolean>();
